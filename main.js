88e2a9d6c5b31c75bff934fd4c9656003a0bd611
Sofia Maria Victoria Menichelli <smenichelli@hotmail.com>
2021-09-11 22:30:51 -0300

    falta poder modificar los inputs de la lista

---
+++ main.js
@@ -0,0 +1,64 @@
+let input = document.querySelector('.input')
+let addBtn = document.querySelector('.addBtn')
+let container = document.querySelector('.container')
+
+class Item {
+    constructor(newTask) {
+        this.createDiv(newTask);
+    }
+    createDiv(input){
+        /* Div item */
+        let inputItem = document.createElement('input')
+        let disabled = inputItem.disabled = true
+        inputItem.classList.add('itemInput')
+        inputItem.value = input.value
+        let div = document.createElement('div')
+        div.classList.add('item')
+        
+        /* Boton editar */
+        let editBtn = document.createElement('button')
+        editBtn.classList.add('editBtn')
+        let iEdit = document.createElement('i')
+        iEdit.classList.add('fad')
+        iEdit.classList.add('fa-edit')
+        editBtn.appendChild(iEdit)
+        
+        /* Boton Borrar */
+        let removeBtn = document.createElement('button')
+        removeBtn.classList.add('removeBtn')
+        let iRemove = document.createElement('i')
+        iRemove.classList.add('fad')
+        iRemove.classList.add('fa-trash-alt')
+        removeBtn.appendChild(iRemove)
+
+        /* anexamos los hijos a container y al div */
+        div.appendChild(inputItem)
+        div.appendChild(editBtn)
+        div.appendChild(removeBtn)
+        container.appendChild(div)
+
+        /* Escuchamos el evento del Click para editar*/
+      /*   editBtn.addEventListener("click", function() {
+           disabled = !disabled
+           inputItem.value.addEventListener('change', function(e) {
+                inputItem.value = e.target.value;
+           })
+        
+        }) */
+
+        /* Escuchamos el evento del Click para Borrar*/
+        removeBtn.addEventListener("click", function() {
+            container.removeChild(div)
+        })
+    }
+}
+function checkInput(){
+    if(input.value != undefined){
+        new Item(input)
+    }
+}
+addBtn.addEventListener("click", function() {
+    checkInput()
+    input.value="";
+}) 
+
